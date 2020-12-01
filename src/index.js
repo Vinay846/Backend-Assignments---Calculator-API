5d9b1085943c2242dc54a49fb52e40ad72908f22
Vinay846 <Vinayvin84629@gmail.com>
2020-12-01 20:02:48 +0530

    ans updated

--- src/index.js
+++ src/index.js
@@ -12,7 +12,123 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 // your code goes here
+app.get("/", (req, res)=>{
+    res.send("Hello world!");
+});
 
+app.post("/add", (req, res)=>{
+    if((typeof(req.body.num1) === 'string' || typeof(req.body.num2) === 'string')){
+        res.send({
+            message: "Invalid data types"
+        })
+    }else{
+        let num1 = req.body.num1;
+        let num2 = req.body.num2;
+        result = num1 + num2;
+        if(result < -1000000 || num1 < -1000000 || num2 < -1000000){
+            res.send({
+                message: "Underflow"
+            })
+        }else if(result > 1000000 || num1 > 100000 || num2 > 100000){
+            res.send({
+                message: "Overflow"
+            })
+        }else{
+            res.send({
+                status: "success",
+                message: "the sum of given two numbers",
+                sum: result
+                });
+        }
+    }
+});
+
+app.post("/sub", (req, res)=>{
+    if((typeof(req.body.num1) === 'string' || typeof(req.body.num2) === 'string')){
+        res.send({
+            message: "Invalid data types"
+        })
+    }else{
+        let num1 = req.body.num1;
+        let num2 = req.body.num2;
+        result = num1 - num2;
+        if(result < -1000000 || num1 < -1000000 || num2 < -1000000){
+            res.send({
+                message: "Underflow"
+            })
+        }else if(result > 1000000 || num1 > 100000 || num2 > 100000){
+            res.send({
+                message: "Overflow"
+            })
+        }else{
+            res.send({
+                status: "success",
+                message: "the sum of given two numbers",
+                sum: result
+                });
+        }
+    }
+});
+
+app.post("/multiply", (req, res)=>{
+    if((typeof(req.body.num1) === 'string' || typeof(req.body.num2) === 'string')){
+        res.send({
+            message: "Invalid data types"
+        })
+    }else{
+        let num1 = req.body.num1;
+        let num2 = req.body.num2;
+        result = num1 * num2;
+        if(result < -1000000 || num1 < -1000000 || num2 < -1000000){
+            res.send({
+                message: "Underflow"
+            })
+        }else if(result > 1000000 || num1 > 100000 || num2 > 100000){
+            res.send({
+                message: "Overflow"
+            })
+        }else{
+            res.send({
+                status: "success",
+                message: "the sum of given two numbers",
+                sum: result
+                });
+        }
+    }
+});
+
+app.post("/divide", (req, res)=>{
+    if((typeof(req.body.num1) === 'string' || typeof(req.body.num2) === 'string')){
+        res.send({
+            message: "Invalid data types"
+        })
+    }else{
+        let num1 = req.body.num1;
+        let num2 = req.body.num2;
+        if(num2 === 0){
+            res.send({
+                status: "error",
+                message: "Cannot divide by zero"
+            })
+        }
+        result = num1 / num2;
+        if(result < -1000000 || num1 < -1000000 || num2 < -1000000){
+            res.send({
+                message: "Underflow"
+            })
+        }else if(result > 1000000 || num1 > 100000 || num2 > 100000){
+            res.send({
+                message: "Overflow"
+            })
+        }else{
+            res.send({
+                status: "success",
+                message: "the sum of given two numbers",
+                sum: result
+                });
+        }
+    }
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
